faab10d37917bdd29335b4bf5750283e8474c3c3
n-inelie <nin3li3@yahoo.com>
2024-07-02 01:27:16 +1200

    added textures

--- src/main.cxx
+++ src/main.cxx
@@ -8,18 +8,27 @@
 #include "shader.hpp"
 #include "utils.hpp"
 
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
 #define HEIGHT 450
 #define WIDTH 800
 
 typedef unsigned int uint;
+typedef unsigned char uchar;
 
 float vertices[] = {
-    // positions         // colors
-    0.5f,  -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,  // bottom right
-    -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,  // bottom left
-    0.0f,  0.5f,  0.0f, 0.0f, 0.0f, 1.0f   // top
+    // positions          // colors           // texture coords
+    0.5f,  0.5f,  0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,  // top right
+    0.5f,  -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,  // bottom right
+    -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,  // bottom left
+    -0.5f, 0.5f,  0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f   // top left
 };
-uint indices[] = {0, 1, 3, 1, 2, 3};
+
+uint indices[] = {
+    0, 1, 3,  // triangle 1
+    1, 2, 3,  // triangle 2
+};
 
 int main(void) {
     glfwInit();
@@ -38,24 +47,59 @@ int main(void) {
 
     glViewport(0, 0, WIDTH, HEIGHT);
 
-    const char* shadersDirPath = getenv("HAZE_SHADERS_PATH");
-    Shader shader(shadersDirPath, "vertex.glsl", "fragment.glsl");
 
-    uint VBO, VAO;
+    const char* shaders_dir_path = getenv("HAZE_SHADERS_PATH");
+    Shader shader(shaders_dir_path, "vertex.glsl", "fragment.glsl");
+
+    uint VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
 
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float),
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
                           (void*)0);
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float),
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
                           (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
+                          (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+
+    uint texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
+                    GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    const char* textures_dir_path = getenv("HAZE_TEXTURES_PATH");
+    char texture1_path[256];
+    sprintf(texture1_path, "%s/%s", textures_dir_path, "texture1.jpg");
+    int width, height, nrChannels;
+    uchar* data = stbi_load(texture1_path, &width, &height, &nrChannels, 0);
+    if (!data) {
+        std::cout << texture1_path << std::endl;
+        std::cout << "Failed to load texture" << std::endl;
+        return -1;
+    }
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB,
+                 GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+
+    stbi_image_free(data);
 
     while (!glfwWindowShouldClose(window)) {
         processInput(window);
@@ -64,8 +108,9 @@ int main(void) {
         glClear(GL_COLOR_BUFFER_BIT);
 
         shader.use();
+        glBindTexture(GL_TEXTURE_2D, texture);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
